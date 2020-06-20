61eeb619ce301f9226ccf09535d0bce12d872931
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-20 15:15:06 +0900

    ビュー追加

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class PostListView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'blog/post_list.html',{})
