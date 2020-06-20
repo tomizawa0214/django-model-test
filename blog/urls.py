34004518b71f00050fbb10b850ef523e52c3f9e7
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-20 17:01:32 +0900

    QuerySet

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
+    # path('<int:pk>/<title>/<text>/', views.PostListView.as_view(), name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import Post
+from django.db.models import Q
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
         # posts = Post.objects.all()
-        posts = Post.objects.get(pk=2)
+        # posts = Post.objects.get(pk=2)
+        # posts = Post.objects.filter(text='テスト')
+        # posts = Post.objects.filter(title='ドラえもん', text='テスト')
+        posts = Post.objects.filter(Q(title='スラムダンク') | Q(text='テスト'))
         return render(request, 'blog/post_list.html',{
             'posts': posts,
         })
