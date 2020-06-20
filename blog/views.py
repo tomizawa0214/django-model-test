25fd9456d82a0053f827a352190832d2d0599b99
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-20 16:11:44 +0900

    テンプレート追加

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Post
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'blog/post_list.html',{})
+        # posts = Post.objects.all()
+        posts = Post.objects.get(pk=2)
+        return render(request, 'blog/post_list.html',{
+            'posts': posts,
+        })
